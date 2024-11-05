3847dab8acc7f355945e68dc1ec4a365fda04304
Yusser96 <115434241+Yusser96@users.noreply.github.com>
2024-11-05 19:00:20 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,4 @@
-﻿// Contact Form Validation and Submission
 
-
-// Smooth Scroll for Navigation Links
-//document.querySelectorAll('a.nav-link').forEach(anchor => {
-   // anchor.addEventListener('click', function (e) {
-     //   e.preventDefault();
-     //   const targetId = this.getAttribute('href');
-      //  const targetElement = document.querySelector(targetId);
-       // const offset = targetElement.offsetTop - document.querySelector('.navbar').offsetHeight;
-        //window.scrollTo({
-           // top: offset,
-          //  behavior: 'smooth'
-      //  });
-    });
-});
 
 // Dark Mode Toggle
 document.getElementById('darkModeSwitch').addEventListener('change', function () {
